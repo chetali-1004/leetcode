b5c08f693f42f71943c469605e4b70f0ebe8a7d3
Chetali Goyal <120305215+chetali-1004@users.noreply.github.com>
2024-09-27 21:17:47 +0530

    Time: 21 ms (39.28%) | Memory: 45.4 MB (10.95%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,36 +1,59 @@
 class Solution {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] adj = new int[n][n];
-        int[] numOfCities = new int[n];
-        int city = -1;
-        int maxCity = n+1;
-
-        for(int[] arr : adj) Arrays.fill(arr, (int)1e8);
-        for(int[] edge : edges){
-            adj[edge[0]][edge[1]] = edge[2];
-            adj[edge[1]][edge[0]] = edge[2];
+    class Pair{
+        int node;
+        int dist;
+        public Pair(int node, int dist){
+            this.node = node;
+            this.dist = dist;
         }
-        for(int i = 0; i<n; i++) adj[i][i] = 0;
-        
-        for(int via = 0; via<n; via++){
-            for(int i = 0; i<n; i++){
-                for(int j = 0; j<n; j++){
-                    if(adj[i][via] == (int)1e8 || adj[via][j] == (int)1e8) continue;
-                    adj[i][j] = Math.min(adj[i][j], adj[i][via] + adj[via][j]);
+    }
+
+    private int n;
+    private int distanceThreshold;
+    private List<List<Pair>> adj;
+
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        this.n = n;
+        this.distanceThreshold = distanceThreshold;
+        this.adj = new ArrayList<>();
+        for(int i = 0; i<n; i++) adj.add(new ArrayList<>());
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(new Pair(edge[1], edge[2]));
+            adj.get(edge[1]).add(new Pair(edge[0], edge[2]));
+        }
+        int res = -1;
+        int minCount = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int count = dijkstra(i);
+            if (count <= minCount) {
+                minCount = count;
+                res = i;
+            }
+        }
+        return res;
+    }
+
+    private int dijkstra(int src){
+        PriorityQueue<Pair> pq = new PriorityQueue<>((p1, p2) -> (p1.dist-p2.dist));
+        int cnt = 0;
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        pq.offer(new Pair(src, 0));
+        while(!pq.isEmpty()){
+            Pair front = pq.poll();
+            int u = front.node;
+            int wt = front.dist;
+            if(wt>dist[u]) continue;
+            cnt++;
+            for(Pair p : adj.get(u)){
+                if(dist[p.node]>wt+p.dist && wt+p.dist<=distanceThreshold){
+                    pq.offer(new Pair(p.node, wt+p.dist));
+                    dist[p.node]=wt+p.dist;
                 }
             }
         }
-
-        for(int i = 0; i<n; i++){
-            int cnt = 0;
-            for(int j = 0; j<n; j++){
-                if(adj[i][j]<=distanceThreshold) cnt++;
-            }
-            if(cnt<=maxCity){
-                maxCity = cnt;
-                city = i;
-            }
-        }
-        return city;
+        return cnt-1;
     }
 }
