97bcbeb1e4e6e417129a0c94d566aa80d6027fd2
Chetali Goyal <120305215+chetali-1004@users.noreply.github.com>
2024-10-01 20:28:37 +0530

    Time: 1 ms (75.48%) | Memory: 45.1 MB (95.10%) - LeetSync

--- 3188-find-champion-i/find-champion-i.java
+++ 3188-find-champion-i/find-champion-i.java
@@ -1,18 +1,20 @@
 class Solution {
     public int findChampion(int[][] grid) {
-        int n = grid.length;
-        int ans = -1;
-        int maxcount = 0;
-        for(int i = 0; i<n; i++){
-            int count = 0;
-            for(int j = 0; j<n; j++){
-                if(grid[i][j]==1) count++;
-            }
-            if(count>maxcount){
-                maxcount = count;
-                ans = i;
+        
+        int strongestSum = 0;
+        int strongestTeam = -1;
+
+        for(int i = 0; i < grid.length; i++){
+            int sum = 0;
+            for (int j = 0; j < grid[0].length; j++){
+                sum+= grid[i][j];
+                if(sum > strongestSum){
+                    strongestSum = sum;
+                    strongestTeam = i;
+                }
             }
         }
-        return ans;
+
+        return strongestTeam;
     }
 }
