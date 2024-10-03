0f9c58c0a1263707b074f5ead979e14d8a03c3af
Chetali Goyal <120305215+chetali-1004@users.noreply.github.com>
2024-10-03 19:39:29 +0530

    Time: 27 ms (39.44%) | Memory: 56.4 MB (61.03%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -1,31 +1,28 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        long totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+        long sum=0;
+        for(int i=0; i<nums.length; i++)
+            sum += nums[i];
+        if(sum%p==0)
+            return 0;
+        
+        int requiredRem=(int)(sum%p);
+
+        HashMap<Integer, Integer> rem = new HashMap<>();
+        rem.put(0, -1);
+        sum=0;
+        int res=nums.length;
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            int curRem=(int)(sum%p);
+            int searchFor=curRem-requiredRem;
+            if(searchFor < 0)
+                searchFor += p;
+            if(rem.containsKey(searchFor))
+                res=Math.min(res, i-rem.get(searchFor));
+            rem.put(curRem ,i);
         }
 
-        // Find remainder when total sum is divided by p
-        int rem = (int)(totalSum % p);
-        if (rem == 0) return 0; // If remainder is 0, no subarray needs to be removed
-
-        HashMap<Integer, Integer> prefixMod = new HashMap<>();
-        prefixMod.put(0, -1);  // Initialize to handle full prefix
-        long prefixSum = 0;
-        int minLength = nums.length;
-
-        for (int i = 0; i < nums.length; ++i) {
-            prefixSum += nums[i];
-            int currentMod = (int)(prefixSum % p);
-            int targetMod = (currentMod - rem + p) % p;
-
-            if (prefixMod.containsKey(targetMod)) {
-                minLength = Math.min(minLength, i - prefixMod.get(targetMod));
-            }
-
-            prefixMod.put(currentMod, i);
-        }
-
-        return minLength == nums.length ? -1 : minLength;
+        return res==nums.length ? -1 : res;
     }
 }
