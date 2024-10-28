8cf7bb8dc0177aea0ca112ffe6e77e0d601527f6
Chetali Goyal <120305215+chetali-1004@users.noreply.github.com>
2024-10-28 13:51:33 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (77.70%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    StringBuilder sb;
+    public String tree2str(TreeNode root) {
+        sb = new StringBuilder();
+        helper(root);
+        return sb.toString();
+    }
+    private void helper(TreeNode root){
+        if(root == null) return;
+        sb.append(root.val);
+
+        if(root.left!=null || root.right!=null){
+            sb.append("(");
+            helper(root.left);
+            sb.append(")");
+
+            if(root.right!=null){
+                sb.append("(");
+                helper(root.right);
+                sb.append(")");
+            }
+        }
+    }
+}
