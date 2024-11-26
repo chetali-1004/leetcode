34e1ad4a2b4681772e59a8cecc5ee91253e74a4d
Chetali Goyal <120305215+chetali-1004@users.noreply.github.com>
2024-11-26 17:43:23 +0530

    Time: 9 ms (97.48%) | Memory: 45.4 MB (89.63%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,29 +1,41 @@
 class Solution {
-    class Pair{
+    class Pair {
         int num;
         int freq;
-        public Pair(int num, int freq){
+
+        public Pair(int num, int freq) {
             this.num = num;
             this.freq = freq;
         }
     }
+
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair p1, Pair p2) -> p2.freq-p1.freq);
-        Map<Integer, Integer> mp = new HashMap<>();
-        int[] sol = new int[k];
-        for(int i = 0; i<nums.length; i++){
-            if(mp.containsKey(nums[i])){
-                mp.put(nums[i], mp.get(nums[i])+1);
+        if (nums.length == 1) return nums; // Handle single-element array edge case
+        
+        // Priority queue for storing elements based on frequency (max-heap)
+        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair p1, Pair p2) -> p2.freq - p1.freq);
+
+        // Sort the array to group the same elements together
+        Arrays.sort(nums);
+
+        // Traverse the sorted array and count frequencies
+        int freq = 1; // Start frequency for the first element
+        for (int i = 1; i <= nums.length; i++) {
+            // If the current element is different or it's the end of the array
+            if (i == nums.length || nums[i] != nums[i - 1]) {
+                pq.offer(new Pair(nums[i - 1], freq)); // Add the previous element and its frequency to the heap
+                freq = 1; // Reset frequency for the next group
+            } else {
+                freq++; // Increment frequency if the same element repeats
             }
-            else mp.put(nums[i], 1);
         }
-        for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
-            pq.offer(new Pair(entry.getKey(), entry.getValue()));
+
+        // Extract the top k elements from the heap
+        int[] sol = new int[k];
+        for (int i = 0; i < k; i++) {
+            sol[i] = pq.poll().num; // Get the element with the highest frequency
         }
-        while(k>0){
-            sol[k-1] = pq.poll().num;
-            k--;
-        }
+
         return sol;
     }
 }
