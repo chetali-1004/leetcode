4b29a2b42d4cf050dd40f319c9786c092f3d372e
Chetali Goyal <120305215+chetali-1004@users.noreply.github.com>
2024-12-16 11:22:50 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (12.28%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,20 +1,13 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
-            if(a[0]==b[0]) return Integer.compare(a[1],b[1]);
-            return Integer.compare(a[0], b[0]);
-        });
-        for(int i = 0; i<nums.length; i++){
-            pq.offer(new int[]{nums[i], i});
-        }
         while(k-->0){
-            int[] top = pq.poll();
-            pq.offer(new int[]{top[0]*multiplier, top[1]});
-        }
-
-        while(!pq.isEmpty()){
-            int[] top = pq.poll();
-            nums[top[1]] = top[0];
+            int i = 0;
+            for(int j = 1; j<nums.length; j++){
+                if(nums[j]<nums[i]){
+                    i = j;
+                }
+            }
+            nums[i] = nums[i]*multiplier;
         }
         return nums;
     }
